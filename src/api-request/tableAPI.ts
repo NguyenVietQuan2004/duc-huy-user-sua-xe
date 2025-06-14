1c5aad6c2837e8b4eae8475ea846921d7c7e7ed1
Nguyenvietquan2004 <nguyenvietquan.30032004@gmail.com>
2025-06-14 21:14:19 +0700

    upload src

--- src/api-request/tableAPI.ts
+++ src/api-request/tableAPI.ts
@@ -3,6 +3,6 @@ import httpRequest from "@/lib/http";
 export const priceServiceApi = {
   // GET /api/admin/table
   getAll() {
-    return httpRequest.get<any>(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/admin/table`);
+    return httpRequest.get<any>(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/admin/price-services`);
   },
 };
