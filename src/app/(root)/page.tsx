7e580800678fe7a9c18b9d127afe90f3277fd31c
NguyenVietQuan2004 <nguyenvietquan.30032004@gmail.com>
2025-07-11 22:37:15 +0700

    upload

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,10 +4,6 @@ import MyService from "./_components/my-service";
 import SixReason from "./_components/six-reason";
 import HomePageClient from "./_components/slider";
 import AdviceSale from "./_components/advice-sales";
-import { categoryApi } from "@/api-request/categoryAPI";
-import A from "./_components/a";
-import B from "./_components/b";
-import C from "./_components/c";
 async function Root() {
   return (
     <div>
@@ -17,9 +13,6 @@ async function Root() {
       <SixReason />
       <AdviceSale />
       <MyPartner />
-      <A></A>
-      <B></B>
-      <C></C>
     </div>
   );
 }
